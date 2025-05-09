ab273efb183232f152ee950c140d241579757609
IsM1ku <89829576+IsM1ku@users.noreply.github.com>
2025-05-09 15:51:49 +0300

    Update ThemeXmlWriter.java

--- ThemeXmlWriter.java
+++ ThemeXmlWriter.java
@@ -1,4 +1,3 @@
-
 package src;
 
 import java.io.*;
@@ -6,19 +5,25 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class ThemeXmlWriter {
-    public static void writeThemeXml(File outDir, String themeName, String author, List<String> bgNames) throws IOException {
+    public static void writeThemeXml(File outDir, String themeName, String author, String version,
+                                     List<String> bgNames, List<Integer> waveTypes,
+                                     String barColor, String indicatorColor,
+                                     String dateColor, String dateLayout,
+                                     String notifyFontColor, String notifyBgColor, String notifyBorderColor,
+                                     boolean includePreviewPage, boolean includePreviewLockscreen) throws IOException {
+
         StringBuilder xml = new StringBuilder();
         xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
         xml.append("<theme format-ver=\"00.99\" package=\"0\">\n");
         xml.append("  <HomeProperty>\n");
         xml.append("    <m_bgParam>\n");
 
-        for (String bg : bgNames) {
+        for (int i = 0; i < bgNames.size(); i++) {
             xml.append("      <BackgroundParam>\n");
-            xml.append("        <m_imageFilePath>").append(bg).append("</m_imageFilePath>\n");
-            String thumb = bg.replace(".png", "t.png");
+            xml.append("        <m_imageFilePath>").append(bgNames.get(i)).append("</m_imageFilePath>\n");
+            String thumb = bgNames.get(i).replace(".png", "t.png");
             xml.append("        <m_thumbnailFilePath>").append(thumb).append("</m_thumbnailFilePath>\n");
-            xml.append("        <m_waveType>11</m_waveType>\n");
+            xml.append("        <m_waveType>").append(waveTypes.get(i)).append("</m_waveType>\n");
             xml.append("      </BackgroundParam>\n");
         }
 
@@ -27,26 +32,28 @@ public class ThemeXmlWriter {
         xml.append("  </HomeProperty>\n");
 
         xml.append("  <InfomationBarProperty>\n");
-        xml.append("    <m_barColor>ff000000</m_barColor>\n");
-        xml.append("    <m_indicatorColor>ffffffff</m_indicatorColor>\n");
+        xml.append("    <m_barColor>").append(convertToARGBIfRGB(barColor)).append("</m_barColor>\n");
+        xml.append("    <m_indicatorColor>").append(convertToARGBIfRGB(indicatorColor)).append("</m_indicatorColor>\n");
         xml.append("  </InfomationBarProperty>\n");
 
         xml.append("  <InfomationProperty>\n");
-        xml.append("    <m_contentVer>01.00</m_contentVer>\n");
+        xml.append("    <m_contentVer>").append(version).append("</m_contentVer>\n");
         xml.append("    <m_provider><m_default>").append(author).append("</m_default></m_provider>\n");
         xml.append("    <m_title><m_default>").append(themeName).append("</m_default></m_title>\n");
         xml.append("    <m_packageImageFilePath>preview_thumbnail.png</m_packageImageFilePath>\n");
-        xml.append("    <m_homePreviewFilePath>preview_page.png</m_homePreviewFilePath>\n");
-        xml.append("    <m_startPreviewFilePath>preview_lockscreen.png</m_startPreviewFilePath>\n");
+        if (includePreviewPage)
+            xml.append("    <m_homePreviewFilePath>preview_page.png</m_homePreviewFilePath>\n");
+        if (includePreviewLockscreen)
+            xml.append("    <m_startPreviewFilePath>preview_lockscreen.png</m_startPreviewFilePath>\n");
         xml.append("  </InfomationProperty>\n");
 
         xml.append("  <StartScreenProperty>\n");
         xml.append("    <m_filePath>start_bg.png</m_filePath>\n");
-        xml.append("    <m_dateColor>ffffffff</m_dateColor>\n");
-        xml.append("    <m_dateLayout>0</m_dateLayout>\n");
-        xml.append("    <m_notifyFontColor>ffffffff</m_notifyFontColor>\n");
-        xml.append("    <m_notifyBgColor>ffffffff</m_notifyBgColor>\n");
-        xml.append("    <m_notifyBorderColor>ffcccccc</m_notifyBorderColor>\n");
+        xml.append("    <m_dateColor>").append(convertToARGBIfRGB(dateColor)).append("</m_dateColor>\n");
+        xml.append("    <m_dateLayout>").append(dateLayout).append("</m_dateLayout>\n");
+        xml.append("    <m_notifyFontColor>").append(convertToARGBIfRGB(notifyFontColor)).append("</m_notifyFontColor>\n");
+        xml.append("    <m_notifyBgColor>").append(notifyBgColor).append("</m_notifyBgColor>\n");
+        xml.append("    <m_notifyBorderColor>").append(notifyBorderColor).append("</m_notifyBorderColor>\n");
         xml.append("  </StartScreenProperty>\n");
 
         xml.append("</theme>\n");
@@ -55,5 +62,14 @@ public class ThemeXmlWriter {
         try (Writer writer = new OutputStreamWriter(new FileOutputStream(xmlFile), StandardCharsets.UTF_8)) {
             writer.write(xml.toString());
         }
+    }
+
+    // Updated format to enforce ARGB consistency
+    public static String toARGB(String rgb) {
+        return "ff" + rgb.toLowerCase(); // prepend full opacity if missing alpha
+    }
+
+    public static String convertToARGBIfRGB(String color) {
+        return (color.length() == 6) ? toARGB(color) : color;
     }
 }
